a9aa81b8de35e817a368d48aef90993ef8302982
Travis Exline <t.exline22@gmail.com>
2019-06-24 17:30:47 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_day_and_season_to_show.rb
+++ db/migrate/009_add_day_and_season_to_show.rb
@@ -1,4 +1,4 @@
-class AddDayAndSeasonToShow < ActiveRecord:Migration[5.1]
+class AddDayAndSeasonToShow < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add column :shows, :season, :integer
